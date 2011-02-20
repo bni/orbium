535e8a09d9579043836553a4d008ab80403d33d9
Björn Nilsson <bni@jsway.se>
2011-02-20 17:27:15 +0100

    Sound fixed for Chrome

--- js/player.js
+++ js/player.js
@@ -1,56 +1,31 @@
 (function(orbium) {
 	orbium.Player = function() {
-		var type = null;
-
-		var supportsPGM = null; // PhoneGap Media support?
-		var supportsSPP = null; // SoundPlug support?
-		var supportsWAV = null; // WAV codec support?
-		var supportsMP3 = null; // MP3 codec support?
-		var supportsOGG = null; // OGG/Vorbis codec support?
+		var hasPhoneGapMedia = null; // PhoneGap Media support?
+		var hasSoundPlug = null; // SoundPlug plugin support?
+		var hasNativeAudio = null; // HTML5 native support?
 
 		this.audioSupported = null; // Audio supported flag
 		this.muted = null; // Muted flag
 
 		this.construct = function() {
-			supportsPGM = false;
-			supportsSPP = false;
-			supportsWAV = false;
-			supportsMP3 = false;
-			supportsOGG = false;
+			hasPhoneGapMedia = false;
+			hasSoundPlug = false;
+			hasNativeAudio = false;
 
-			if (orbium.Util.isPG() && (orbium.Util.isUA("iPhone") || orbium.Util.isUA("iPad"))) {
-				supportsSPP = true;
+			if ((orbium.Util.isUA("iPhone") || orbium.Util.isUA("iPad")) &&
+				orbium.Util.isPG()) {
+				hasSoundPlug = true;
 			} else if (orbium.Util.isPG()) {
-				supportsPGM = true;
+				hasPhoneGapMedia = true;
 			} else 	if (!!document.createElement("audio").canPlayType) {
 				var a = document.createElement("audio");
 
 				if (!!(a.canPlayType && a.canPlayType('audio/wav; codecs="1"').replace(/no/, ''))) {
-					supportsWAV = true;
-				}
-
-				if (!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
-					supportsMP3 = true;
-				}
-
-				if (!!(a.canPlayType && a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''))) {
-					supportsOGG = true;
+					hasNativeAudio = true;
 				}
 			}
 
-			if (supportsPGM) {
-				type = "wav";
-			} else if (supportsSPP) {
-				type = "wav";
-			} else if (supportsWAV) {
-				type = "wav";
-			} else if (supportsMP3) {
-				type = "mp3";
-			} else if (supportsOGG) {
-				type = "ogg";
-			}
-
-			if (supportsPGM || supportsSPP || supportsWAV || supportsMP3 || supportsOGG) {
+			if (hasPhoneGapMedia || hasSoundPlug || hasNativeAudio) {
 				this.audioSupported = true;
 				this.muted = false;
 			} else {
@@ -59,11 +34,8 @@
 			}
 
 			// Disable sounds on certain devices and cases that are problematic
-			if (((orbium.Util.isUA("iPhone") || orbium.Util.isUA("iPad")) && !orbium.Util.isPG()) ||
-				orbium.Util.isUA("webOS") ||
-				orbium.Util.isUA("Android") ||
-				orbium.Util.isUA("MSIE") ||
-				orbium.Util.isUA("Chrome")) {
+			if (((orbium.Util.isUA("iPhone") || orbium.Util.isUA("iPad")) &&
+				!orbium.Util.isPG()) || orbium.Util.isUA("Android")) {
 				this.audioSupported = false;
 				this.muted = true;
 			}
@@ -71,12 +43,13 @@
 
 		this.play = function(sound) {
 			if (this.audioSupported && !this.muted) {
-				if (supportsPGM) {
-					new Media("snd/"+type+"/"+sound+"."+type).play();
-				} else if (supportsSPP) {
-					PhoneGap.exec("SoundPlug.play", sound+"."+type);
-				} else {
-					new Audio("snd/"+type+"/"+sound+"."+type).play();
+				if (hasPhoneGapMedia) {
+					// Disabled for now since it doesnt work
+					//new Media("/android_asset/"+sound+".wav").play();
+				} else if (hasSoundPlug) {
+					PhoneGap.exec("SoundPlug.play", sound+".wav");
+				} else if (hasNativeAudio) {
+					new Audio("snd/wav/"+sound+".wav").play();
 				}
 			}
 		};
